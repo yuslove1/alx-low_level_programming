828e116e9271f9b05c34486f4920a5200d05f059
yuslove1 <adexyuslove@gmail.com>
2023-02-16 17:51:56 -0800

    6. Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	double d;
+	float f;
+	char c;
+
+	printf("size of int :%lu. \n"sizeof(i));
+	printf("size of double :%lu. \n"sizeof(d));
+	printf("size of float :%lu. \n"sizeof(f));
+	printf("size of chat :%lu. \n"sizeof(c));
+
+	return (0);
+}
