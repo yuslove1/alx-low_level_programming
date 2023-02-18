5eaab15d6b26203004ead837b43f19e2aa306f46
yuslove1 <adexyuslove@gmail.com>
2023-02-17 16:38:13 -0800

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
- * main: Entry of the block
+ * main - Entry of the block
  * description - this code genarate random number
+ * Return: Always 0
  */
 int main(void)
 {
