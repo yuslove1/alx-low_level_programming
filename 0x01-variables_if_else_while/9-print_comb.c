c7a79655b8348b895f6a80ff19edad521867e43f
yuslove1 <adexyuslove@gmail.com>
2023-02-19 09:54:19 -0800

    9. Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	putchar('0' + x);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
