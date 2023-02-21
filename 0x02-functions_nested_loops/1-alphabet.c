a3c0c7bde0ea5decbce783ad3e4e01f9c879b627
yuslove1 <adexyuslove@gmail.com>
2023-02-21 15:54:59 -0800

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - This function print alphabet
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
