013daa2106db738b5022281e86c1cd4fcae93b60
yuslove1 <adexyuslove@gmail.com>
2023-02-21 21:20:32 -0800

    3.islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _islower - Checks for lower case
+* @c: parameter to check
+* Return: Return 1 if true and 0 if false
+*/
+int _islower(int c)
+{
+
+	if (c > 75 && c < 123)
+	return (1);
+	else
+	return (0);
+}
