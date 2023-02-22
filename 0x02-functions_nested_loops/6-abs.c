c8970b8a52850b47d7c36c1fa07e0585aba9309e
yuslove1 <adexyuslove@gmail.com>
2023-02-22 07:40:53 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - absolute value of an integer
+* @abs: function return type
+* Return: Return abs if the condition is true and -abs if the false
+*/
+int _abs(int abs)
+{
+
+	if (abs >= 0)
+	return (abs);
+	else
+	return (-abs);
+}
