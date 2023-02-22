26f9d72206fd27e97394a49f951eb881c52a88ce
yuslove1 <adexyuslove@gmail.com>
2023-02-22 15:13:42 -0800

    10. a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - this find sum of ts
+ *@num1: Parameter
+ *@num2: parameter2
+ * Return: value of sum
+ */
+
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
