163dbe5ce439ea480cabffa4dcd3b57b1896f057
yuslove1 <adexyuslove@gmail.com>
2023-02-23 14:47:17 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - check for uppercase alphabet
+* @c: parameter
+* Return: return 1 if c is uppercase and 0 otherwise
+*/
+
+int _isupper(int c);
+{
+	int alpha;
+
+	if (alpha >= 'A' && alpha <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
