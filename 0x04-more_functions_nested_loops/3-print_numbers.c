448737557246266b5607b5409f9f968085c6018e
yuslove1 <adexyuslove@gmail.com>
2023-02-23 15:48:11 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_numbers - check for uppercase alphabet
+*
+* Return: 0 always
+*/
+
+void print_numbers(void)
+{
+	int i
+
+	for (i = 0; i <= 9; i++)
+	{
+	_putchar(i +'0');
+	}
+	putchar('\n');
+	return (0);
+}
