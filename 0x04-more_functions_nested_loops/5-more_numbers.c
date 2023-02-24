b71d667d6b824f111a8510010abfce98348adc26
yuslove1 <adexyuslove@gmail.com>
2023-02-23 18:11:40 -0800

    5. Number constitute

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,24 @@
 
 void more_numbers(void)
 {
-	int i, j;
-	int tens, unit;
+	int i;
+	int j;
+	int tens;
+	int unit;
 
 	for (i = 1; i <= 10; i++)
 	{
-	for (j = 0; j <= 14; i++)
+	for (j = 0; j <= 14; j++)
 	{
 	tens = j / 10;
 	unit = j % 10;
 
 	if (j > 9)
-		_putchar(unit + '0');
-	_putchat(tens + '0');
+	{
+	_putchar(tens + '0');
+	}
+	_putchar(unit + '0');
 	}
 	_putchar('\n');
 	}
-
 }
