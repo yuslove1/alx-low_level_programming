2a82edc71bc56d942d89eaf506f3e0140b561b47
yuslove1 <adexyuslove@gmail.com>
2023-02-27 13:45:50 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,5 +8,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
