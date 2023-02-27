38b069c12f294a5a43fdfe8dde4420c65e85ccff
yuslove1 <adexyuslove@gmail.com>
2023-02-27 13:01:45 -0800

    0.98 Battery st.

---
+++ 0x05-pointers_arrays_string/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+* _putchar - write character c to stdout
+* @c - charater to print
+* Return: return 1 on success and -1 on error
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
