ec2c89998900e727867e998da02ab33fd7e60c59
yuslove1 <adexyuslove@gmail.com>
2023-02-27 19:05:56 -0800

    correction

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
 *_puts - prints a string
 *@str: A pointer to an int that will be changed
 *
 *Return: void which means our answer is correct
 */
-
 void _puts(char *str)
 {
 	char *c;
@@ -15,7 +15,7 @@ void _puts(char *str)
 
 	for (m = 0; c[m]; m++)
 	{
-	_putchar (c[m]);
+	_putchar(c[m]);
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,7 +2,7 @@
 
 /**
 * _putchar - write character c to stdout
-* @c - charater to print
+* @c: charater to print
 * Return: return 1 on success and -1 on error
 */
 
@@ -10,5 +10,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-
