8d9fb7a392d893745a9257a06036ee7ae793ff20
yuslove1 <adexyuslove@gmail.com>
2023-03-10 09:27:30 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the function
+ * @argc: the count
+ * @argv: the array of string
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
