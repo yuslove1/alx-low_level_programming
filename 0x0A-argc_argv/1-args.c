4eab33ddba51e9e07d8f987b307b844e7bc7ed08
yuslove1 <adexyuslove@gmail.com>
2023-03-10 09:45:47 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * @argv: the array of string
  * Return: 0 always
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
