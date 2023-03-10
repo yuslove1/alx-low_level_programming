72c1de072d0142c1b5388668b767d510182392f9
yuslove1 <adexyuslove@gmail.com>
2023-03-10 14:15:57 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,13 +6,8 @@
  * @argv: the array of string
  * Return: 0 always
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	int i;
-
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+	printf("%s\n", argv[0]);
 	return (0);
 }
