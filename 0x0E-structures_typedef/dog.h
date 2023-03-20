f27d5ff7eb1f65240e0c01210180c21d3ef81451
yuslove1 <adexyuslove@gmail.com>
2023-03-20 13:37:02 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,10 +14,4 @@ struct dog
 	char *owner;
 };
 
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-void free_dog(dog_t *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
