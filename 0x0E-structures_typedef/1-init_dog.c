6ec53c83295334dfc981c2fa951d185382f6129c
yuslove1 <adexyuslove@gmail.com>
2023-03-20 14:45:50 -0700

    task 1 corre.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - this function initialize a variable type struct dog
@@ -9,7 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
