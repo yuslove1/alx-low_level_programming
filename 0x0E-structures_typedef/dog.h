741afbe7f6e9ac5878a41ddb3b2e9a132fc9d708
yuslove1 <adexyuslove@gmail.com>
2023-03-20 19:10:56 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+int _putchar (char c);
 /**
  * struct dog - data structure
- * @name: name is the dog
- * @age: age of the dog
- * @owner: the owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog with name, age, and owner's name.
  */
 struct dog
 {
@@ -14,4 +16,9 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
