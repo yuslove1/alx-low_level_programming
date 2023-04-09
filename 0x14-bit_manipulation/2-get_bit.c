6cb13b13104049c4400c24ea4e12b56f6be2e180
yuslove1 <adexyuslove@gmail.com>
2023-04-09 04:18:49 -0700

    task 2 coreection

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_value;
 
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index >= 64)
 		return (-1);
 
 		bit_value = (n >> index) & 1;
