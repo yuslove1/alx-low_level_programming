e4265747a6efbb8850b95a00c1e4272562e214bf
yuslove1 <adexyuslove@gmail.com>
2023-04-09 07:32:32 -0700

    updated task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 if worked or -1 if error occured
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= 64)
 		return (-1);
